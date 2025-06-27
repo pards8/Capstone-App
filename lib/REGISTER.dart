687c84b839368e96c0521a73283d2e38dc698a2e
YeshaRelevo <princesstumbaga2@gmail.com>
2025-06-27 20:59:15 +0800

    register

---
+++ lib/REGISTER.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello, Flutter!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
