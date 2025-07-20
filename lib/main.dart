267b927eac9eabf65e39a02f3ddc7ad47b75ea6e
Xai-3N1AL3 <xyronncahayon28@gmail.com>
2025-07-20 15:30:25 +0800

    Week 4 day 1 to 5

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const delivery_login.LoginScreen(),
+      home: const DeliveryInterface(),
     );
   }
 }
